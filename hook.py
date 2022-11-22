6b8bdccb83f95b804b3d4c6ff120859d432338f8
Tristan B. Velloza Kildaire <deavmi@disroot.org>
2022-11-22 10:48:54 +0200

    Formatted

--- hook.py
+++ hook.py
@@ -2,43 +2,50 @@
 
 from flask import request, Flask
 from flask.logging import logging
-from flask import render_template
+from flask import request
+import subprocess
+import os
 
 # Setup the Flask web app.
 app = Flask("thing")
 
 commands={
-        "homepage" : {
-                "dir":"/home/pi/HDD/temp/homepage",
-                "pre-command":["git", "pull"],
-                "command":["hugo"]
-        },
-        "crxn" : {
-                "dir":"/home/pi/HDD/temp/crxn",
-                "pre-command":["git", "pull"],
-                "command": ["mkdocs", "build"]
-        },
-        "bnet": {
-                "dir":"/home/pi/HDD/temp/bnet",
-                "pre-command":["git", "pull"],
-                "command": ["mkdocs", "build"]
-        }
+	"homepage" : {
+		"dir": "/home/pi/HDD/temp/homepage",
+		"pre-command": ["git", "pull"],
+		"command": ["hugo"]
+	},
+	"crxn" : {
+		"dir": "/home/pi/HDD/temp/crxn",
+		"pre-command": ["git", "pull"],
+		"command": ["mkdocs", "build"]
+	},
+	"bnet": {
+		"dir": "/home/pi/HDD/temp/bnet",
+		"pre-command": ["git", "pull"],
+		"command": ["mkdocs", "build"]
+	}
 }
-
 
 @app.route("/build/<site>", methods=["POST"])
 def buildHandler(site):
-        from flask import request
-        print(request.headers)
-        print("Requesting an automatic rebuild of '%s'"%(site))
+	print("Requesting an automatic rebuild of '%s'"%(site))
 
-        item=commands[site]
-        itemDir=item["dir"]
-        itemCommand=item["command"]
+	# Extract the correct mapping
+	item=commands[site]
+	itemDir=item["dir"]
+	itemPreCommand=item["pre-command"]
+	itemCommand=item["command"]
 
-        import subprocess
-        import os
-        os.chdir(itemDir)
-        subprocess.call(itemCommand)
+	# Change directory to the item's CWD
+	os.chdir(itemDir)
 
-        return "Bruh"
+	# Call the pre-command followed by the command
+	subprocess.call(itemPreCommand)
+	subprocess.call(itemCommand)
+
+	# The deadline will probably be exceeded but flask
+	# wants this here
+	return None
+
+app.run(host="::")
